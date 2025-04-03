076b8e6c95777fc4ff4e4fca723fa37a9d65d7f9
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-04-03 16:35:37 -0500

    Update writeAppointment 3 Abril 2

--- OldFiles/writeAppointment.py
+++ OldFiles/writeAppointment.py
@@ -2,44 +2,40 @@ import json
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from bson import ObjectId
+from fhir.resources.appointment import Appointment
 
-# Función para conectar a la base de datos MongoDB
-def connect_to_mongodb(uri, db_name, collection_name):
+def connect_to_mongodb():
+    """ Conecta a la base de datos MongoDB """
+    uri = "mongodb+srv://mardugo:clave@sampleinformationservic.t2yog.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
     client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client[db_name]
-    collection = db[collection_name]
-    return collection
+    db = client["HIS"]
+    return db["appointments"]
 
-# Función para guardar la cita médica en MongoDB
 def save_appointment_to_mongodb(appointment_data, collection):
     try:
-        # Verificar que appointment_data ya sea un diccionario
+        # Verificar que appointment_data sea un diccionario
         if isinstance(appointment_data, str):
             appointment_data = json.loads(appointment_data)
 
-        # Insertar el documento en la colección de MongoDB
-        result = collection.insert_one(appointment_data)
-        
-        # Retornar el ID del documento insertado como string
+        # Validar el formato FHIR del Appointment
+        appointment = Appointment.model_validate(appointment_data)
+        validated_data = appointment.model_dump()
+
+        # Insertar el documento validado en MongoDB
+        result = collection.insert_one(validated_data)
         return str(result.inserted_id)
     except Exception as e:
         print(f"Error al guardar en MongoDB: {e}")
         return None
 
-# Función principal de escritura de cita médica
 def WriteAppointment(appointment_json):
-    uri = "mongodb+srv://mardugo:clave@sampleinformationservic.t2yog.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
-    db_name = "HIS"
-    collection_name = "appointments"
-    
-    # Conectar a la colección de MongoDB
-    collection = connect_to_mongodb(uri, db_name, collection_name)
-    
-    # Guardar la cita en MongoDB
+    """ Función para registrar una cita médica en MongoDB """
+    collection = connect_to_mongodb()
     inserted_id = save_appointment_to_mongodb(appointment_json, collection)
     
     if inserted_id:
         return "success", inserted_id
     else:
         return "error", None
+
 
