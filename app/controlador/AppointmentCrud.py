5cbcfd3b9e57a3faab62c4fca9af169e2cf0d08c
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-04-03 17:20:30 -0500

    Update AppointmentCrud 222

--- app/controlador/AppointmentCrud.py
+++ app/controlador/AppointmentCrud.py
@@ -17,22 +17,26 @@ def GetAppointmentById(appointment_id: str):
 
 def GetAppointmentByIdentifier(patientSystem, patientValue):
     try:
-        appointment = collection.find_one({"participant.actor.identifier.system": patientSystem, 
-                                          "participant.actor.identifier.value": patientValue})
+        appointment = collection.find_one({
+            "participant.actor.identifier.system": patientSystem, 
+            "participant.actor.identifier.value": patientValue
+        })
         if appointment:
             appointment["_id"] = str(appointment["_id"])
             return "success", appointment
         return "notFound", None
     except Exception as e:
         return "notFound", None        
-    
+
 def WriteAppointment(appointment_dict: dict):
     try:
         appt = Appointment.model_validate(appointment_dict)
     except Exception as e:
+        print("Error de validación en Appointment:", e)  # <-- Agregado para depuración
         return f"errorValidating: {str(e)}", None
+
     validated_appointment_json = appt.model_dump()
-    result = collection.insert_one(appointment_dict)
+    result = collection.insert_one(validated_appointment_json)  # <-- Guarda el objeto validado
     if result:
         inserted_id = str(result.inserted_id)
         return "success", inserted_id
